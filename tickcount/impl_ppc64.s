d4127fed17824ca421e969e30154d0470cabf717
Alexander Knopov <sknopov@gmail.com>
2025-05-04 17:56:35 -0400

    Fix for own impl

--- tickcount/impl_ppc64.s
+++ tickcount/impl_ppc64.s
@@ -1,7 +1,21 @@
 #include "textflag.h"
 
-// Not tested!
+// ChatGPT solution - not tested!
 
+// func TickCountA() uint64
+TEXT ·TickCountA(SB), NOSPLIT, $0-8
+Loop:
+    MFSPR R4, 269     // TBU -> R4
+    MFSPR R5, 268     // TBL -> R5
+    MFSPR R6, 269     // TBU again -> R6
+    CMP   R4, R6
+    BNE   Loop        // Retry if TBU changed
+
+    RLDIMI R4, R5, 32, 0  // Insert R5 into low 32 bits of R4
+    MOVD   R4, ret+0(FP)  // Store result to return value
+    RET
+
+/* Alt
 TEXT ·TickCountA(SB),7,$0
 		MFTBU R3
 		MFTBL R4
@@ -9,4 +23,5 @@ TEXT ·TickCountA(SB),7,$0
 		SLWI R3,R3,32
 		OR R3,R3,R4
 		STW R3, 0(5)
-		RET
+		BLR
+*/
