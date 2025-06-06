5234b0c426ea074f9a7d81a84fb5b9e2327928fc
Alexander Knopov <alexander@knopovdesign.com>
2025-06-06 12:26:30 -0400

    Implemented t-test analysis of performance changes (#4)
    * Implemented t-test analysis of performance changes
    
    * Fixed types; hid ttest internal structures and functions
    Simplified ttest_test.go
    
    * Using float64 for statistic calculations
    
    ---------
    
    Co-authored-by: Alexander Knopov <sknopov@gmail.com>

--- perform_test.go
+++ perform_test.go
@@ -70,20 +70,19 @@ func TestOneTaskFailure(t *testing.T) {
 func TestCalcStats(t *testing.T) {
 	assertT := assert.New(t)
 
-	aFixture := taskFixture{runtimes: []time.Duration{0, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000}, fails: 3}
+	aFixture := taskFixture{runtimes: []time.Duration{9000000, 8000000, 7000000, 6000000, 5000000, 4000000, 3000000, 2000000, 1000000, 0}, fails: 3}
 	stats := calcStats([]*taskFixture{&aFixture})
 
 	assertT.Equal(1, len(stats))
 	oneStat := stats[0]
 	assertT.Equal(10, oneStat.Count)
-	assertT.Equal(time.Duration(45000000), oneStat.TotalTime)
-	assertT.Equal(time.Duration(0), oneStat.MinTime)
-	assertT.Equal(time.Duration(4500000), oneStat.AvgTime)
-	assertT.Equal(time.Duration(5000000), oneStat.MedTime)
-	assertT.Equal(time.Duration(9000000), oneStat.MaxTime)
-	assertT.Equal(time.Duration(3027650), oneStat.StdDev)
 	assertT.Equal(3, oneStat.Fails)
-	assertT.Equal(aFixture.runtimes, oneStat.Values)
+	assertT.Equal(0.0, oneStat.MinTime)
+	assertT.Equal(4.5, oneStat.AvgTime)
+	assertT.Equal(5.0, oneStat.MedTime)
+	assertT.Equal(9.0, oneStat.MaxTime)
+	assertT.Equal(3.0276503540974917, oneStat.StdDev)
+	assertT.Equal([]float64{9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0}, oneStat.Values)
 }
 
 func TestRunTest(t *testing.T) {
@@ -103,7 +102,7 @@ func TestRunTest(t *testing.T) {
 	assertT.Equal(1, len(stats))
 	oneStat := stats[0]
 	assertT.Equal(TotalTests, oneStat.Count)
-	assertT.GreaterOrEqual(oneStat.MinTime, SleepTime)
+	assertT.GreaterOrEqual(oneStat.MinTime, float64(SleepTime)/msecFctr)
 }
 
 func TestAssertNoErr(t *testing.T) {
@@ -118,4 +117,39 @@ func TestAssumeOnErr(t *testing.T) {
 
 	assertT.Equal(1, AssumeOnErr(func() (int, error) { return 1, nil }, -1))
 	assertT.Equal(-1, AssumeOnErr(func() (int, error) { return 0, errTest }, -1))
+}
+
+var (
+	stat1 = RunStats{Count: 100,
+		AvgTime: 563.619080,
+		MinTime: 523.003400,
+		MaxTime: 763.448900,
+		MedTime: 566.497100,
+		StdDev:  563.61908,
+	}
+	stat2 = RunStats{Count: 200,
+		AvgTime: 638.293246,
+		MinTime: 617.243500,
+		MaxTime: 694.757900,
+		MedTime: 635.208100,
+		StdDev:  12.563879,
+	}
+)
+
+func TestCalcPvals(t *testing.T) {
+	assertT := assert.New(t)
+
+	probs, err := CalcPvals([]RunStats{stat1}, []RunStats{stat2})
+	assertT.NoError(err)
+	assertT.Equal(0.9691777120698255, probs[0])
+
+	stat2.Count = 1
+	_, err = CalcPvals([]RunStats{stat1}, []RunStats{stat2})
+	assertT.ErrorContains(err, "invalid statistics data in test #0")
+	assertT.ErrorContains(err, "sample is too small")
+
+	stats1 := []RunStats{RunStats{}}
+	stats2 := []RunStats{RunStats{}, RunStats{}}
+	_, err = CalcPvals(stats1, stats2)
+	assertT.ErrorContains(err, "different size of tasks")
 }
